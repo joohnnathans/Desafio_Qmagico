21b41f4a8a16595133814084e6f7cbfd902fd5c2
joohnnathans <joohnnathans@gmail.com>
2019-04-12 17:41:49 -0300

    remove iterator for

--- desafioQmagico.py
+++ desafioQmagico.py
@@ -2,8 +2,7 @@
 def piramide(a):
     b = a-1
     c = 1
-    for i in range(a):
-        while b > -1: 
-            print ("%s %s %s" % (('_' * b), ('*' * c), ('_' * b)))
-            b -= 1
-            c += 2
+    while b > -1: 
+        print ("%s %s %s" % (('_' * b), ('*' * c), ('_' * b)))
+        b -= 1
+        c += 2
